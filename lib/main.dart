d73eca35927840d071073cbc5cb9fc44f2075ac2
Akash Morwal <akashbhau101@gmail.com>
2025-04-17 13:55:20 +0530

    Initial commit  and also add the Fature of DatePicker and Time Picker

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
+      ),
+      home: const MyHomePage(title: 'DateTime Picker'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 400,
+          color: Colors.orange,
+          child: Column(
+            children: [
+              Text(
+                'Select Both',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  DateTime? datePicked = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2010),
+                    lastDate: DateTime(2026),
+                  );
+
+                  if (datePicked != null) {
+                    print("Selected Date : ${datePicked.year}: ${datePicked.month}: ${datePicked.day}");
+                  }
+                },
+                child: Text(
+                  'Select Date',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ),
+              ElevatedButton(onPressed: () async {
+                TimeOfDay? timePicked = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.now(),
+                initialEntryMode: TimePickerEntryMode.dial);
+
+                if(timePicked != null){
+                  print("Selected Time : ${timePicked.hour}:${timePicked.minute}");
+                }
+              }, child: Text(
+                'Select Time',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),)
+            ],
+          ),
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
